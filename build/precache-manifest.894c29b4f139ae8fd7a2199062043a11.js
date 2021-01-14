57a536f38cd043c4899bcfc6cdee2d26531897bb
Ivan Golubey <ig2108@yahoo.com>
2021-01-14 16:07:48 +0200

    update build

---
+++ build/precache-manifest.894c29b4f139ae8fd7a2199062043a11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89a14931619f889b8509cfa8309d504",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5548460b37c5dff71bae",
+    "url": "/static/css/main.51e3c40a.chunk.css"
+  },
+  {
+    "revision": "d207c9a0c9bab6d2522d",
+    "url": "/static/js/2.ab7ecdd5.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.ab7ecdd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5548460b37c5dff71bae",
+    "url": "/static/js/main.bee55573.chunk.js"
+  },
+  {
+    "revision": "6b8e3a7dc93efaae37d0",
+    "url": "/static/js/runtime-main.252c2776.js"
+  }
+]);
